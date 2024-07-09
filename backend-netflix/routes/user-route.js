eb5fa5f5a1767930a4c6bf63664964a8d7ba891f
GARAPATI TEJA SRI <148844350+Arnella16@users.noreply.github.com>
2024-07-09 17:57:47 +0530

    Update user-route.js

--- backend-netflix/routes/user-route.js
+++ backend-netflix/routes/user-route.js
@@ -3,6 +3,6 @@ import { Register } from "../controllers/user.js";
 
 const userRouter=express.Router();
 
-userRouter.route("/register"). post(Register);
+userRouter.route("/register").post(Register);
 
 export default userRouter;
