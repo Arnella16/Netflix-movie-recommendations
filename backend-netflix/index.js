94012878b3c97d8ef4bcc9bd60ab90518ea7a857
GARAPATI TEJA SRI <148844350+Arnella16@users.noreply.github.com>
2024-07-09 14:28:22 +0530

    Update index.js

--- backend-netflix/index.js
+++ backend-netflix/index.js
@@ -3,7 +3,8 @@ import express from "express";
 import dotenv from "dotenv";
 import databaseConnection from "./utils/database.js";
 import cookieParser from "cookie-parser";
-import userRoute from "./routes/user-route.js"
+import userRouter from "./routes/user-route.js";
+import cors from "cors";
 
 databaseConnection();
 
@@ -15,19 +16,20 @@ const app = express();
 app.use(express.urlencoded({extended:true}));
 app.use(express.json());
 app.use(cookieParser());
+app.use(cors());
 
-app.get("/", (req,res)=>{
-    res.status(200).json({
-        message:"Hello"
-    })
-})
+// app.get("/", (req,res)=>{
+//     res.status(200).json({
+//         message:"Hello"
+//     })
+// })
 
 app.post('/api/v1/user/register', (req, res) => {
     // Handle registration logic here
     res.send('User registered successfully');
 });
 
-app.use("/api/v1/user" , userRoute);
+app.use("/api/v1/user" , userRouter);
 
 app.listen(process.env.PORT, ()=>{
     console.log(`Server at port ${process.env.PORT}`);
