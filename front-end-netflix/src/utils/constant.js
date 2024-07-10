46d8901bbe1332d190176be4b1a62211c4b0a84d
GARAPATI TEJA SRI <148844350+Arnella16@users.noreply.github.com>
2024-07-11 00:59:35 +0530

    Add files via upload

---
+++ front-end-netflix/src/utils/constant.js
@@ -0,0 +1,10 @@
+export const options = {
+	method: 'GET',
+	hostname: 'imdb-top-100-movies.p.rapidapi.com',
+	port: null,
+	path: '/',
+	headers: {
+		'x-rapidapi-key': '8a248f9c53msh7fee0cb3b47bdbbp15b375jsn1d0bc33620a6',
+		'x-rapidapi-host': 'imdb-top-100-movies.p.rapidapi.com'
+	}
+};
